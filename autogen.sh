d3c482b92bc688c37a1679ed4d813a5918d29e85
rfeany <rfeany>
2007-03-06 10:40:28 +0000

    don't try to generate debian/deleteable.files - this is the original source tree!

--- autogen.sh
+++ autogen.sh
@@ -39,31 +39,6 @@ test -d debian && {
 		rm -f debian/executable.files
 		find -type f -perm +111 ! -name '.*' -fprint debian/executable.files
 	}
-
-	# Remove any files in upstream tarball that we don't have in the Debian
-	# package (because diff cannot remove files)
-	version=`dpkg-parsechangelog | awk '/Version:/ { print $2 }' | sed -e 's/-[^-]\+$//'`
-	source=`dpkg-parsechangelog | awk '/Source:/ { print $2 }' | tr -d ' '`
-	if test -r ../${source}_${version}.orig.tar.gz ; then
-		echo Generating list of files that should be removed...
-		rm -f debian/deletable.files
-		touch debian/deletable.files
-		[ -e debian/tmp ] && rm -rf debian/tmp
-		mkdir debian/tmp
-		( cd debian/tmp ; tar -zxf ../../../${source}_${version}.orig.tar.gz )
-		find debian/tmp/ -type f ! -name '.*' -print0 | xargs -0 -ri echo '{}' | \
-		  while read -r i ; do
-			if test -e "${i}" ; then
-				filename=$(echo "${i}" | sed -e 's#.*debian/tmp/[^/]\+/##')
-				test -e "${filename}" || echo "${filename}" >>debian/deletable.files
-			fi
-		  done
-		rm -fr debian/tmp
-	else
-		echo Emptying list of files that should be deleted...
-		rm -f debian/deletable.files
-		touch debian/deletable.files
-	fi
 }
 
 exit 0

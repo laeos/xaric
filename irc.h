27f0b7713975b7e7b5594428107b8cb4b69f56a1
Rex Feany <rfeany@rexfeany.com>
2026-09-13 02:26:47 -0700

    make autogen.sh quiet with modern autotools
    - run AC_USE_SYSTEM_EXTENSIONS before the first compiler test;
      AC_SEARCH_LIBS([strerror],[cposix]) linking before it tripped the
      ordering warning
    - drop AC_HEADER_STDC (nothing uses STDC_HEADERS) and AC_HEADER_TIME;
      irc.h now includes <sys/time.h> when available followed by <time.h>,
      the pattern autoconf recommends over the TIME_WITH_SYS_TIME dance
    - drop the AC_HEADER_STDC requirement from the vendored sa.ac typedef
      check
    
    autoreconf --force --install is now warning-free with autoconf 2.73 /
    automake 1.18, and the regenerated configure still builds the tree
    clean.

--- irc.h
+++ irc.h
@@ -30,16 +30,10 @@ extern char *line_thing;
 #include <signal.h>
 #include <sys/param.h>
 
-#ifdef TIME_WITH_SYS_TIME
+#ifdef HAVE_SYS_TIME_H
 # include <sys/time.h>
-# include <time.h>
-#else
-# ifdef HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif				/* HAVE_SYS_TIME_H */
-#endif				/* TIME_WITH_SYS_TIME */
+#endif
+#include <time.h>
 
 #ifdef HAVE_SYS_FCNTL_H
 # include <sys/fcntl.h>

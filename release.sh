16907c5c77a4461ff04d4bb7d090844390e2dc5c
Rex Feany <rfeany@rexfeany.com>
2026-09-19 21:45:02 -0700

    bump version to 0.13.10, fix release.sh

--- release.sh
+++ release.sh
@@ -42,7 +42,7 @@ md5sum xaric-${V}.tar.gz > xaric-${V}.tar.gz.md5
 sed -e "s/VERSION/${V}/g" -e "s/RELEASEDATE/${today}/" ${root}/www/index.html > index.html
 
 host=rfeany@tonya.fnordsoft.com
-rr=~xaric
+rr=/home/domains/xaric.org
 sw=${rr}/software/xaric/releases/
 idx=${rr}/index.html
 
